1ea5b39bbf68dd29e08c7852ccea065a9ed288b5
Jaydeep Baldaniya <jaydeepbaldaniya08@gmail.com>
2024-09-21 00:00:27 +0530

    Writing some code in React

---
+++ first-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ first-app/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return <div className="footer">No CopyRight</div>
+}
+
+export default Footer
